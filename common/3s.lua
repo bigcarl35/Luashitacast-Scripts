cda7b7bf32cabeb6090addccb8474c926fcb5656
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2025-12-18 12:34:27 -0500

    Add files via upload

---
+++ common/3s.lua
@@ -0,0 +1,17 @@
+--Zone: Manaclipper
+--Zone ID: 3
+return {
+    Names = {
+        ['Clot'] = { Name='Clot', 
+        ['Colossal Calamari'] = { Name='Colossal Calamari', 
+        ['Cutter'] = { Name='Cutter', 
+        ['Cyclopean Conch'] = { Name='Cyclopean Conch', 
+        ['Fatty Pugil'] = { Name='Fatty Pugil', 
+        ['Ghost Crab'] = { Name='Ghost Crab', 
+        ['Greater Pugil'] = { Name='Greater Pugil', 
+        ['Harajnite'] = { Name='Harajnite', 
+        ['Kraken'] = { Name='Kraken', 
+        ['Uragnite'] = { Name='Uragnite', 
+        ['Zoredonite'] = { Name='Zoredonite', 
+    },
+};
