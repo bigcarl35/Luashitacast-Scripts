be78eb55c5c8aedcee2cf24319e39a0cf1b2dc28
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2025-12-18 12:47:24 -0500

    Add files via upload

---
+++ common/MobDB/10.lua
@@ -0,0 +1,9 @@
+--Zone: The Shrouded Maw
+--Zone ID: 10
+return {
+    Names = {
+        ['Diabolos']            = { Name='Diabolos', Ecosystem='races', Family='avatars' },
+        ['Diremite']            = { Name='Diremite', Ecosystem='vermin', Family='diremites' },
+        ['Diremite Dominator']  = { Name='Diremite Dominator', Ecosystem='vermin', Family='diremites' },
+        ['Pasuk']               = { Name='Pasuk', Ecosystem='vermin', Family='diremites' },
+};
