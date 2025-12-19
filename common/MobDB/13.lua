dd602590d821d7410eab7bb65651354abe32b696
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2025-12-19 12:09:22 -0500

    Add files via upload

---
+++ common/MobDB/13.lua
@@ -0,0 +1,26 @@
+--Zone: Mine Shaft #2716
+--Zone ID: 13
+return {
+    Names = {
+        ['Bugbby']                  = { Name='Bugbby', Ecosystem='beastmen', Family='bugbears' },
+        ['Bugboy']                  = { Name='Bugboy', Ecosystem='beastmen', Family='bugbears' },
+        ['Chekochuk']               = { Name='Chekochuk', Ecosystem='beastmen', Family='goblins' },
+        ['Elvaan Automaton']        = { Name='Elvaan Automaton', Ecosystem='humanoids', Family='elvaans' },
+        ['Fantoccini']              = { Name='Fantoccini', Ecosystem='humanoids', Family='humans' },
+        ['Fantoccini Automaton']    = { Name='Fantoccini Automaton', Ecosystem='unclassified', Family='automatons' },
+        ['Fantoccini Avatar']       = { Name='Fantoccini Avatar', Ecosystem='unclassified', Family='avatars' },
+        ['Fantoccini Monster']      = { Name='Fantoccini Monster', Ecosystem='unclassified', Family='monsters' },
+        ['Fantoccini Wyvern']       = { Name='Fantoccini Wyvern', Ecosystem='dragons', Family='wyverns' },
+        ['Galkan Automaton']        = { Name='Galkan Automaton', Ecosystem='unclassified', Family='galkans' },
+        ['Hume Automaton']          = { Name='Hume Automaton', Ecosystem='unclassified', Family='humes' },
+        ['Mithran Automaton']       = { Name='Mithran Automaton', Ecosystem='unclassified', Family='mithrans' },
+        ['Moblin Clergyman']        = { Name='Moblin Clergyman', Ecosystem='beastmen', Family='moblins' },
+        ['Moblin Fantocciniman']    = { Name='Moblin Fantocciniman', Ecosystem='beastmen', Family='moblins' },
+        ['Moblin Wisewoman']        = { Name='Moblin Wisewoman', Ecosystem='beastmen', Family='moblins' },
+        ['Movamuq']                 = { Name='Movamuq', Ecosystem='beastmen', Family='moblins' },
+        ['Swipostik']               = { Name='Swipostik', Ecosystem='beastmen', Family='moblins' },
+        ['Tarutaru Automaton']      = { Name='Tarutaru Automaton', Ecosystem='unclassified', Family='tarutarus' },
+        ['Trikotrak']               = { Name='Trikotrak', Ecosystem='beastmen', Family='moblins' },
+        ['Twilotak']                = { Name='Twilotak', Ecosystem='beastmen', Family='moblins' },
+    },
+};
