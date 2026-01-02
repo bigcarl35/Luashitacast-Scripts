7febdb38eef37dcfde717f585a7b5ffb18a3eeed
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-02 13:25:21 -0500

    Add files via upload

---
+++ common/MobDB/180.lua
@@ -0,0 +1,14 @@
+--Zone: La'Loff Amphitheater
+--Zone ID: 180
+return {
+    Names = {
+        ['Ark Angel EV'] = { Name='Ark Angel EV', Ecosystem='humanoids', Family='elvaan' },
+        ['Ark Angel GK'] = { Name='Ark Angel GK', Ecosystem='humanoids', Family='galka' },
+        ['Ark Angel HM'] = { Name='Ark Angel HM', Ecosystem='humanoids', Family='humes' },
+        ['Ark Angel MR'] = { Name='Ark Angel MR', Ecosystem='humanoids', Family='mithra' },
+        ['Ark Angel TT'] = { Name='Ark Angel TT', Ecosystem='humanoids', Family='tarutaru' },
+        ['Ark Angel\'s Mandragora'] = { Name='Ark Angel\'s Mandragora', Ecosystem='plantoids', Family='mandragoas' },
+        ['Ark Angel\'s Tiger'] = { Name='Ark Angel\'s Tiger', Ecosystem='beasts', Family='tigers' },
+        ['Ark Angel\'s Wyvern'] = { Name='Ark Angel\'s Wyvern', Ecosystem='dragons', Family='pet wyverns' },
+    },
+};
---
+++ common/MobDB/181.lua
@@ -0,0 +1,9 @@
+--Zone: The Celestial Nexus
+--Zone ID: 181
+return {
+    Names = {
+        ['Eald\'narche'] = { Name='Eald\'narche', Ecosystem='humanoids', Family='zilart' },
+        ['Exoplates'] = { Name='Exoplates', Ecosystem='structures', Family='structures' },
+        ['Orbital'] = { Name='Orbital', Ecosystem='arcana', Family='spheroids' },
+    },
+};
