5c2bbe801be24e8e3fb02c41899577bd0dac5d84
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-10 15:07:58 -0500

    Add files via upload

---
+++ common/MobDB/31.lua
@@ -0,0 +1,21 @@
+--Zone: Monarch Linn
+--Zone ID: 31
+return {
+    Names = {
+        ['Earth Elemental'] = { Name='Earth Elemental', Ecosystem='elementals', Family='elementals' },
+        ['Guard Hippogryph'] = { Name='Guard Hippogryph', Ecosystem='birds', Family='hippogryphs' },
+        ['Gullin Baelfyr'] = { Name='Gullin Baelfyr', Ecosystem='elementals', Family='elementals' },
+        ['Gullin Byrgen'] = { Name='Gullin Byrgen', Ecosystem='elementals', Family='elementals' },
+        ['Gullin Gefyrst'] = { Name='Gullin Gefyrst', Ecosystem='elementals', Family='elementals' },
+        ['Gullin Ungeweder'] = { Name='Gullin Ungeweder', Ecosystem='elementals', Family='elementals' },
+        ['Gullinkambi'] = { Name='Gullinkambi', Ecosystem='birds', Family='rocs' },
+        ['Hamadryad'] = { Name='Hamadryad', Ecosystem='plantoids', Family='flytraps' },
+        ['Hotupuku'] = { Name='Hotupuku', Ecosystem='lizards', Family='bugards' },
+        ['Mammet Master'] = { Name='Mammet Master', Ecosystem='arcana', Family='mammets' },
+        ['Mammet-19 Epsilon'] = { Name='Mammet-19 Epsilon', Ecosystem='arcana', Family='mammets' },
+        ['Mammet-800'] = { Name='Mammet-800', Ecosystem='arcana', Family='mammets' },
+        ['Ouryu'] = { Name='Ouryu', Ecosystem='dragons', Family='wyrms' },
+        ['Razon'] = { Name='Razon', Ecosystem='arcana', Family='clusters' },
+        ['Watch Hippogryph'] = { Name='Watch Hippogryph', Ecosystem='birds', Family='hippogryphs' },
+    },
+};
---
+++ common/MobDB/32.lua
@@ -0,0 +1,13 @@
+--Zone: Sealion's Den
+--Zone ID: 32
+return {
+    Names = {
+        ['Cherukiki'] = { Name='Cherukiki', Ecosystem='humanoids', Family='tarutaru' },
+        ['Kukki-Chebukki'] = { Name='Kukki-Chebukki', Ecosystem='humanoids', Family='tarutaru' },
+        ['Makki-Chebukki'] = { Name='Makki-Chebukki', Ecosystem='humanoids', Family='tarutaru' },
+        ['Mammet-22 Zeta'] = { Name='Mammet-22 Zeta', Ecosystem='arcana', Family='mammets' },
+        ['Omega'] = { Name='Omega', Ecosystem='unclassified', Family='biotecchnological weapons' },
+        ['Tenzen'] = { Name='Tenzen', 
+        ['Ultima'] = { Name='Ultima', 
+    },
+};
