121740ca5f13a51005bdfae71de9a8619bb228b4
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-10 12:44:42 -0500

    Delete common/21.lua

--- common/21.lua
+++
@@ -1,9 +0,0 @@
---Zone: Spire of Mea
---Zone ID: 21
-return {
-    Names = {
-        ['Delver'] = { Name='Delver', Ecosystem='cravers', Family='empties'},
-        ['Envier'] = { Name='Envier', Ecosystem='cravers', Family='empties'},
-        ['Seether'] = { Name='Seether', Ecosystem='seethers', Family='empties'},
-    },
-};
