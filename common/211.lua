1c12e842a103d9d9e6c1bb892a2edc5776a03092
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-10 12:44:53 -0500

    Delete common/211.lua

--- common/211.lua
+++
@@ -1,8 +0,0 @@
---Zone: Cloister of Tides
---Zone ID: 211
-return {
-    Names = {
-        ['Leviathan Prime'] = { Name='Leviathan Prime', Ecosystem='unclassified', Family='avatars'},
-        ['Water Elemental'] = { Name='Water Elemental', Ecosystem='elementals', Family='elementals'},
-    },
-};
