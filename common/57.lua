b7174aa72901531bc4eddd845f53c0cc83e95786
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-11 15:03:42 -0500

    Add files via upload

---
+++ common/57.lua
@@ -0,0 +1,17 @@
+--Zone: Talacca Cove
+--Zone ID: 57
+return {
+    Names = {
+        ['Angler Orobon'] = { Name='Angler Orobon', Ecosystem='aquans', Family='orobons'},
+        ['Arrapago Leech'] = { Name='Arrapago Leech', Ecosystem='amorphs', Family='leeches'},
+        ['Gessho'] = { Name='Gessho', Ecosystem='beastmen', Family='yagudo'},
+        ['Giant Orobon'] = { Name='Giant Orobon', Ecosystem='aquans', Family='orobons'},
+        ['Imp Bandsman'] = { Name='Imp Bandsman', Ecosystem='demons', Family='imps'},
+        ['Lahama'] = { Name='Lahama', Ecosystem='aquans', Family='sea monks'},
+        ['Llamhigyn Y Dwr'] = { Name='Llamhigyn Y Dwr', Ecosystem='undead', Family='doomed'},
+        ['Qultada'] = { Name='Qultada', Ecosystem='humanoids', Family='elvaan'},
+        ['Talacca Clot'] = { Name='Talacca Clot', Ecosystem='amorphs', Family='slimes'},
+        ['Valkeng'] = { Name='Valkeng', Ecosystem='unclassified', Family='automatons'},
+        ['Wootzshell'] = { Name='Wootzshell', Ecosystem='aquans', Family='crabs'},
+    },
+};
