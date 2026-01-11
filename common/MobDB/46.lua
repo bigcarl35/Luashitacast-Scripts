be4b2d3d50be1e93c12b9c35191b49f5dc497ced
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-11 14:07:18 -0500

    Add files via upload

---
+++ common/MobDB/46.lua
@@ -0,0 +1,17 @@
+--Zone: Open sea route to Al Zahbi
+--Zone ID: 46
+return {
+    Names = {
+        ['Blanched Kraken'] = { Name='Blanched Kraken', Ecosystem='aquans', Family='sea monks'},
+        ['Gugru Crab'] = { Name='Gugru Crab', Ecosystem='aquans', Family='crabs'},
+        ['Gugru Jagil'] = { Name='Gugru Jagil', Ecosystem='aquans', Family='pugils'},
+        ['Gugru Orobon'] = { Name='Gugru Orobon', Ecosystem='aquans', Family='orobons'},
+        ['Northern Piranu'] = { Name='Northern Piranu', Ecosystem='aquans', Family='orobons'},
+        ['Ocean Jagil'] = { Name='Ocean Jagil', Ecosystem='aquans', Family='pugils'},
+        ['Ocean Kraken'] = { Name='Ocean Kraken', Ecosystem='aquans', Family='sea monks'},
+        ['Passage Crab'] = { Name='Passage Crab', Ecosystem='aquans', Family='crabs'},
+        ['Revenant'] = { Name='Revenant', Ecosystem='undead', Family='ghosts'},
+        ['Thunder Elemental'] = { Name='Thunder Elemental', Ecosystem='elementals', Family='elementals'},
+        ['Water Elemental'] = { Name='Water Elemental', Ecosystem='elementals', Family='elementals'},
+    },
+};
---
+++ common/MobDB/47.lua
@@ -0,0 +1,17 @@
+--Zone: Open sea route to Mhaura
+--Zone ID: 47
+return {
+    Names = {
+        ['Blanched Kraken'] = { Name='Blanched Kraken', Ecosystem='aquans', Family='sea monks'},
+        ['Gugru Crab'] = { Name='Gugru Crab', Ecosystem='aquans', Family='crabs'},
+        ['Gugru Jagil'] = { Name='Gugru Jagil', Ecosystem='aquans', Family='pugils'},
+        ['Gugru Orobon'] = { Name='Gugru Orobon', Ecosystem='aquans', Family='orobons'},
+        ['Ocean Jagil'] = { Name='Ocean Jagil', Ecosystem='aquans', Family='pugils'},
+        ['Ocean Kraken'] = { Name='Ocean Kraken', Ecosystem='aquans', Family='sea monks'},
+        ['Passage Crab'] = { Name='Passage Crab', Ecosystem='aquans', Family='crabs'},
+        ['Revenant'] = { Name='Revenant', Ecosystem='undead', Family='ghosts'},
+        ['Southern Piranu'] = { Name='Southern Piranu', Ecosystem='aquans', Family='orobons'},
+        ['Thunder Elemental'] = { Name='Thunder Elemental', Ecosystem='elementals', Family='elementals'},
+        ['Water Elemental'] = { Name='Water Elemental', Ecosystem='elementals', Family='elementals'},
+    },
+};
