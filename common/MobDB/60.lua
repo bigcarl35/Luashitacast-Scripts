208df9ad68cf5750debb4ae1859b11c38d59b1d6
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-12 11:16:05 -0500

    Add files via upload

---
+++ common/MobDB/60.lua
@@ -0,0 +1,11 @@
+--Zone: The Ashu Talif
+--Zone ID: 60
+return {
+    Names = {
+        ['Ashu Talif Captain'] = { Name='Ashu Talif Captain', Ecosystem='undead', Family='fomors' },
+        ['Ashu Talif Crew'] = { Name='Ashu Talif Crew', Ecosystem='undead', Family='fomors' },
+        ['Gessho'] = { Name='Gessho', Ecosystem='beastmen', Family='yagudo' },
+        ['Gowam'] = { Name='Gowam', Ecosystem='humanoids', Family='galka' },
+        ['Yazquhl'] = { Name='Yazquhl', Ecosystem='humanoids', Family='humes' },
+    },
+};
---
+++ common/MobDB/67.lua
@@ -0,0 +1,22 @@
+--Zone: Jade Sepulcher
+--Zone ID: 67
+return {
+    Names = {
+        ['Blacktattoo Vedool Ja'] = { Name='Blacktattoo Vedool Ja', Ecosystem='beastmen', Family='mamool ja' },
+        ['Jasweem'] = { Name='Jasweem', Ecosystem='humanoids', Family='humes' },
+        ['Lancelord Gaheel Ja'] = { Name='Lancelord Gaheel Ja', Ecosystem='beastmen', Family='mamool ja' },
+        ['Mamool Ja Flamerearer'] = { Name='Mamool Ja Flamerearer', Ecosystem='beastmen', Family='mamool ja' },
+        ['Mamool Ja Isangoma'] = { Name='Mamool Ja Isangoma', Ecosystem='beastmen', Family='mamool ja' },
+        ['Mamool Ja Polemicist'] = { Name='Mamool Ja Polemicist', Ecosystem='beastmen', Family='mamool ja' },
+        ['Mamool Ja Profligate'] = { Name='Mamool Ja Profligate', Ecosystem='beastmen', Family='mamool ja' },
+        ['Mamool Ja Unseen'] = { Name='Mamool Ja Unseen', Ecosystem='beastmen', Family='mamool ja' },
+        ['Mocking Colibri'] = { Name='Mocking Colibri', Ecosystem='birds', Family='birds' },
+        ['Phantom Puk'] = { Name='Phantom Puk', Ecosystem='dragons', Family='puks' },
+        ['Raubahn'] = { Name='Raubahn', Ecosystem='humanoids', Family='humes' },
+        ['Scorched-snout Wyvern'] = { Name='Scorched-snout Wyvern', 
+        ['Shadowhand Kajeel Ja'] = { Name='Shadowhand Kajeel Ja', Ecosystem='beastmen', Family='mamool ja' },
+        ['Tiyaash'] = { Name='Tiyaash', Ecosystem='humanoids', Family='humes' },
+        ['Wabjahl'] = { Name='Wabjahl', Ecosystem='humanoids', Family='humes' },
+        ['Whitetattoo Rahool Ja'] = { Name='Whitetattoo Rahool Ja', Ecosystem='beastmen', Family='mamool ja' },
+    },
+};
