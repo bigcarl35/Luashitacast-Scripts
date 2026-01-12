6b2f4233a1f6d1216189c70c5623869178420cfd
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-12 10:31:58 -0500

    Add files via upload

---
+++ common/MobDB/59.lua
@@ -0,0 +1,21 @@
+--Zone: Silver Sea route to Al Zahbi
+--Zone ID: 59
+return {
+    Names = {
+        ['Abyssal Pugil'] = { Name='Abyssal Pugil', Ecosystem='aquans', Family='pugils'},
+        ['Air Elemental'] = { Name='Air Elemental', Ecosystem='elementals', Family='elementals'},
+        ['Almighty Apkallu'] = { Name='Almighty Apkallu', Ecosystem='birds', Family='apkallu'},
+        ['Apkallu'] = { Name='Apkallu', Ecosystem='birds', Family='apkallu'},
+        ['Bathybic Kulshedra'] = { Name='Bathybic Kulshedra', Ecosystem='aquans', Family='sea monks'},
+        ['Bigclaw'] = { Name='Bigclaw', Ecosystem='aquans', Family='crabs'},
+        ['Cyan Deep Crab'] = { Name='Cyan Deep Crab', Ecosystem='aquans', Family='crabs'},
+        ['Cyan Deep Pugil'] = { Name='Cyan Deep Pugil', Ecosystem='aquans', Family='pugils'},
+        ['Imp'] = { Name='Imp', Ecosystem='demons', Family='imps'},
+        ['Kulshedra'] = { Name='Kulshedra', Ecosystem='aquans', Family='sea monks'},
+        ['Orobon'] = { Name='Orobon', Ecosystem='aquans', Family='orobons'},
+        ['Submarine Nipper'] = { Name='Submarine Nipper', Ecosystem='aquans', Family='crabs'},
+        ['Thalassic Pugil'] = { Name='Thalassic Pugil', Ecosystem='aquans', Family='pugils'},
+        ['Thunder Elemental'] = { Name='Thunder Elemental', Ecosystem='elementals', Family='elementals'},
+        ['Utukku'] = { Name='Utukku', Ecosystem='undead', Family='ghosts'},
+    },
+};
