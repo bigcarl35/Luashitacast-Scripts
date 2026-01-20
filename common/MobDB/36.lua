807554ab300d4c01ea281f7d3287a1fc650c0751
bigcarl35 <18159038+bigcarl35@users.noreply.github.com>
2026-01-20 18:57:24 -0500

    Add files via upload

---
+++ common/MobDB/36.lua
@@ -0,0 +1,10 @@
+--Zone: Empyreal Paradox
+--Zone ID: 36
+return {
+    Names = {
+        ['Eald\'narche'] = { Name='Eald\'narche', Ecosystem='humanoids', Family='zilart'},
+        ['Kam\'lanaut'] = { Name='Kam\'lanaut', Ecosystem='humanoids', Family='zilart'},
+        ['Metus'] = { Name='Metus', Ecosystem='unclassified', Family='supreme beings'},
+        ['Promathia'] = { Name='Promathia', Ecosystem='unclassified', Family='supreme beings'},
+    },
+};
---
+++ common/MobDB/63.lua
@@ -0,0 +1,23 @@
+--Zone: Lebros Cavern
+--Zone ID: 63
+return {
+    Names = {
+        ['Black Shuck'] = { Name='Black Shuck', Ecosystem='beasts', Family='cerberuses'},
+        ['Brittle Rock'] = { Name='Brittle Rock', Ecosystem='structures', Family='structures'},
+        ['Broken Troll Soldier'] = { Name='Broken Troll Soldier', Ecosystem='beastmen', Family='trolls'},
+        ['Crimson Eruca'] = { Name='Crimson Eruca', Ecosystem='vermin', Family='crawlers'},
+        ['Dahak'] = { Name='Dahak', Ecosystem='dragongs', Family='dragons'},
+        ['Jorporbor the Hellraker'] = { Name='Jorporbor the Hellraker', Ecosystem='beastmen', Family='trolls'},
+        ['Nocuous Inferno'] = { Name='Nocuous Inferno', Ecosystem='elementals', Family='elementals'},
+        ['Old Troll'] = { Name='Old Troll', Ecosystem='beastmen', Family='trolls'},
+        ['Qiqirn Ceramist'] = { Name='Qiqirn Ceramist', Ecosystem='beastmen', Family='qiqirns'},
+        ['Qiqirn Eggler'] = { Name='Qiqirn Eggler', Ecosystem='beastmen', Family='qiqirns'},
+        ['Qiqirn Volcanist'] = { Name='Qiqirn Volcanist', Ecosystem='beastmen', Family='qiqirns'},
+        ['Ranch Wamoura'] = { Name='Ranch Wamoura', Ecosystem='vermin', Family='wamouras'},
+        ['Ranch Wamouracampa'] = { Name='Ranch Wamouracampa', Ecosystem='vermin', Family='wamouracampas'},
+        ['Troll Combatant'] = { Name='Troll Combatant', Ecosystem='beastmen', Family='trolls'},
+        ['Volcanic Bomb'] = { Name='Volcanic Bomb', Ecosystem='arcana', Family='bombs'},
+        ['Vulcanian Bomb'] = { Name='Vulcanian Bomb', Ecosystem='arcana', Family='bombs'},
+        ['Wamouracampa'] = { Name='Wamouracampa', Ecosystem='vermin', Family='wamouracampas'},
+    },
+};
